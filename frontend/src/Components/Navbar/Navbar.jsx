cad90d73ba07d33ebb99a870a731e5754be31d94
aditya-1967 <aditya.win.official@gmail.com>
2023-12-12 20:29:41 +0530

    navbar-done

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -1,22 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Navbar.css'
 import logo from '../Assets/images/logo/de Compras.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCartShopping } from '@fortawesome/free-solid-svg-icons'
 
 const Navbar = () => {
+
+    const [menu, setMenu] = useState("home");
+
     return (
         <div className='navbar'>
             <div className="nav-logo">
                 <img src={logo} alt="" />
             </div>
             <ul className="nav-links">
-                <li><span className='pages'>Home</span></li>
-                <li><span className='pages'>Shop</span></li>
-                <li><span className='pages'>Blog</span></li>
-                <li><span className='pages'>About</span></li>
-                <li><span className='pages'>Contact Us</span></li>
-                <li><span classname='cart-icon'><FontAwesomeIcon icon={faCartShopping} className='cart-icon'/></span></li>
+                <li onClick={() => {setMenu("home")}}><span className={`pages ${menu === "home" ? 'active-page' : ''}`}>Home</span></li>
+                <li onClick={() => {setMenu("shop")}}><span className={`pages ${menu === "shop" ? 'active-page' : ''}`}>Shop</span></li>
+                <li onClick={() => {setMenu("blog")}}><span className={`pages ${menu === "blog" ? 'active-page' : ''}`}>Blog</span></li>
+                <li onClick={() => {setMenu("about")}}><span className={`pages ${menu === "about" ? 'active-page' : ''}`}>About</span></li>
+                <li onClick={() => {setMenu("contact")}}><span className={`pages ${menu === "contact" ? 'active-page' : ''}`}>Contact Us</span></li>
+                <li onClick={() => {setMenu("cart")}}><span className={`cart-icon ${menu === "cart" ? 'active-page' : ''}`}><FontAwesomeIcon icon={faCartShopping} className='cart-icon'/></span></li>
             </ul>
         </div>
     )
